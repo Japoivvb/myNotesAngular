21fa52ddf1bcc7f79975d2cb5109d81f923a7f9d
JPortugal <jose.portugal@stucom.com>
2024-07-06 18:25:53 +0200

    adds add note component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './header/header.component';
+import { AddNoteComponent } from './add-note/add-note.component';
 import { NotesListComponent } from './notes-list/notes-list.component';
 import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, NotesListComponent, FormsModule],
+  imports: [RouterOutlet, HeaderComponent, NotesListComponent, FormsModule, AddNoteComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
