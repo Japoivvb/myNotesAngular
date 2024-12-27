3074c3158b4899c757034ea522152f2449005afb
Japoivvb <jose.portugal@stucom.com>
2024-12-27 12:35:21 +0100

    add employee with form

--- src/app/employees/employees.component.ts
+++ src/app/employees/employees.component.ts
@@ -3,24 +3,36 @@ import { RouterModule } from '@angular/router';
 import { EmployeeComponent } from '../employee/employee.component';
 import { Employee } from '../employee.model';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 // decorator
 @Component({
   selector: 'app-employees',
   standalone: true,
   // adding employee inside this component
-  imports: [RouterModule,EmployeeComponent,CommonModule],
+  imports: [RouterModule, EmployeeComponent, CommonModule, FormsModule],
   templateUrl: './employees.component.html',
   styleUrl: './employees.component.css'
 })
 // to be visible the component
 export class EmployeesComponent {
-  employees:Employee[]=[
+  
+  formName: string = "";
+  formAge: number = 0;
+  formSurname: string = "";
+  formCompany: string = "";
+  
+  employees: Employee[] = [
     new Employee("Alan", "Arranz", 21, "Amazon"),
     new Employee("Biel", "Bonas", 22, "BMW"),
     new Employee("Cris", "Cruz", 23, "Celeritas")
-];
+  ];
+  
+  constructor() {
+  }
 
-  constructor(){
+  addEmployee() {
+     let employee= new Employee(this.formName,this.formSurname, this.formAge, this.formCompany);
+     this.employees.push(employee);
   }
 }
