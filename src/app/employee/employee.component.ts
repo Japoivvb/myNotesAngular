0eeacdfd001282a55ae10c9da02d2f5dcfb7462f
Japoivvb <jose.portugal@stucom.com>
2024-12-27 13:29:31 +0100

    comm parent2child @Input

--- src/app/employee/employee.component.ts
+++ src/app/employee/employee.component.ts
@@ -1,6 +1,7 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { Employee } from '../employee.model';
 
 @Component({
   selector: 'app-employee',
@@ -10,71 +11,76 @@ import { FormsModule } from '@angular/forms';
   styleUrl: './employee.component.css'
 })
 export class EmployeeComponent {
-  name = "Juan";
-  surname = "Diaz";
-  age = 22;
-  company = "Acc";
-  departments: any[];
-
-  companyName = false;
-  registered = false;
-  registerText = "there is no user registered";
-
-  constructor() {
-    this.departments = [
-      { skill: 'Backend' },
-      { skill: 'Frontend' },
-      { skill: 'Fullstack' },
-    ];
-  }
-
-  getDepartments(){     
-
-  }
-
-  updateCompany(value: string) {
-    this.company = value;
-  }
-
-  showCompany() {
-    alert(this.company)
-  }
-
-  getRegistered() {
-    // data from ddbb
-    this.registered = false
-  }
-
-  // event binding
-  setRegistered(event: Event) {
-    // data from ddbb
-    if ((<HTMLInputElement>event.target).value == "true") {
-      alert("the user has been registered");
-      this.registerText = "the user has been registered";
-
-    } else {
-      this.registerText = "there is no user registered";
-
-    }
-  }
-
-  register() {
-    // alert("the user has been registered");  
-    this.registerText = "the user has been updated";
-    this.registered = true;
-  }
+  @Input() emp:Employee;//passing data from parent to child
+  @Input() idx:number;
 
 
 
-  setCompany(event: Event) {
-    // data from ddbb
-    this.company = (<HTMLInputElement>event.target).value;
-  }
+  // name = "Juan";
+  // surname = "Diaz";
+  // age = 22;
+  // company = "Acc";
+  // departments: any[];
 
-  setName(event: Event) {
-    // data from ddbb
-    this.name = (<HTMLInputElement>event.target).value;
-  }
+  // companyName = false;
+  // registered = false;
+  // registerText = "there is no user registered";
+
+  // constructor() {
+  //   this.departments = [
+  //     { skill: 'Backend' },
+  //     { skill: 'Frontend' },
+  //     { skill: 'Fullstack' },
+  //   ];
+  // }
+
+  // getDepartments(){     
+
+  // }
+
+  // updateCompany(value: string) {
+  //   this.company = value;
+  // }
+
+  // showCompany() {
+  //   alert(this.company)
+  // }
+
+  // getRegistered() {
+  //   // data from ddbb
+  //   this.registered = false
+  // }
+
+  // // event binding
+  // setRegistered(event: Event) {
+  //   // data from ddbb
+  //   if ((<HTMLInputElement>event.target).value == "true") {
+  //     alert("the user has been registered");
+  //     this.registerText = "the user has been registered";
+
+  //   } else {
+  //     this.registerText = "there is no user registered";
+
+  //   }
+  // }
+
+  // register() {
+  //   // alert("the user has been registered");  
+  //   this.registerText = "the user has been updated";
+  //   this.registered = true;
+  // }
+
+
+
+  // setCompany(event: Event) {
+  //   // data from ddbb
+  //   this.company = (<HTMLInputElement>event.target).value;
+  // }
+
+  // setName(event: Event) {
+  //   // data from ddbb
+  //   this.name = (<HTMLInputElement>event.target).value;
+  // }
 
 
 
